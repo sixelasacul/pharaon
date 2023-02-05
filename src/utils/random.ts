7f67b5bb5e3e14157b335b5f644c32f79d413c1d
Alexis LUCAS <sixela.sacul.pro@protonmail.com>
2023-02-05 16:05:08 +0000

    Nit

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,7 +1,7 @@
 export function getRandomIntInclusive(min: number, max: number) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1) + min)
+  const intMin = Math.ceil(min)
+  const intMax = Math.floor(max)
+  return Math.floor(Math.random() * (intMax - intMin + 1) + intMin)
 }
 
 export function getUniqueRandomIntListInclusive(min: number, max: number, count: number) {
