4e88c099a4691e7fe02229a3f03ce88c1f4bcba0
Alexis Lucas <a.lucas@lepermislibre.fr>
2023-10-16 23:03:15 +0200

    Initial draft for marking tempo

--- src/components/Syllable/Syllable.tsx
+++ src/components/Syllable/Syllable.tsx
@@ -1,37 +1,51 @@
-import { clsx } from 'clsx'
+import { cva, type VariantProps } from 'class-variance-authority'
 import { type Color } from '../../state/PickedColorContext'
+import { cn } from '@/utils/cn'
 
-interface SyllableProps {
+const syllableVariants = cva('select-all transition-colors', {
+  variants: {
+    size: {
+      normal: 'duration-75',
+      large: 'duration-150'
+    }
+  }
+})
+
+type SyllableVariants = VariantProps<typeof syllableVariants>
+
+interface SyllableProps extends Omit<SyllableVariants, 'tempo'> {
   color: Color
   className?: string
-  size?: 'normal' | 'large'
+  tempo?: number
   onClick(): void
 }
+
+// Word component to solve issues with inline-flex?
 
 export function Syllable({
   className,
   children,
   color,
   size = 'normal',
+  tempo,
   onClick
 }: React.PropsWithChildren<SyllableProps>) {
+  const hasTempo = tempo !== undefined
   // TODO: Fix overlapping padding
   return (
-    <span
-      className={clsx(
-        'select-all py-2 transition-colors',
-        color.base,
-        color.hover,
-        {
-          'py-1 duration-75': size === 'normal',
-          'py-2 duration-150': size === 'large'
-        },
-        className
-      )}
-      role='button'
-      onClick={onClick}
-    >
-      {children}
+    <span className='inline-flex flex-col items-center justify-start'>
+      <span
+        className={cn(
+          color.base,
+          color.hover,
+          syllableVariants({ size, className })
+        )}
+        role='button'
+        onClick={onClick}
+      >
+        {children}
+      </span>
+      {hasTempo && <span>{tempo}</span>}
     </span>
   )
 }
